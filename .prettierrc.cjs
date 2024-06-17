afe91d2b842eb914a8e04082229adc97fec5039c
pktherock <59223750+pktherock@users.noreply.github.com>
2024-06-17 12:05:14 +0530

    enable eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5', // todo
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',// todo
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div>
       <h1>Shadi solution tms ui</h1>
+      <Wrapper />
     </div>
-  )
+  );
 }
 
-export default App
+const Wrapper = () => <h2>wrapper component</h2>;
+
+export default App;

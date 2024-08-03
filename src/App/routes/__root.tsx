07a013e95cad7b1eaa595394bbcbcfcc077a2208
pktherock <59223750+pktherock@users.noreply.github.com>
2024-08-03 23:49:27 +0530

    configure tanstack router

---
+++ src/App/routes/__root.tsx
@@ -0,0 +1,13 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+export default rootRoute;
